ab0ef6fe4c483bfc6369f65f0643c8e4c1d5c7a6
dixie24 <rpamg255@gmail.com>
2025-12-02 23:58:33 +0600

    hallo2.2

--- main.py
+++ main.py
@@ -17,3 +17,12 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.post("/items/")
 def create_item(name: str, price: float):
     return {"name": name, "price": price}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, name: str, price: float):
+    return {"item_id": item_id, "name": name, "price": price}   
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):  
+    return {"item_id": item_id, "status": "deleted"}
